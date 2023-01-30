df71a90577cbabdd807314c1307d9199ad780d39
Palacios Tarrillo Christian <85316618+PalaciosTarrilloChristian@users.noreply.github.com>
2023-01-30 16:50:27 -0500

    Guard Activo

--- src/app/pages/pages-routing.module.ts
+++ src/app/pages/pages-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {PersonajesComponent} from "./personajes/personajes.component";
+import {AuthGuard} from "../guards/auth.guard";
 
 const routes: Routes = [
   {
     path: 'personajes',
+    canActivate: [ AuthGuard ],
     component: PersonajesComponent
   },
   {
--- src/app/services/usuarios.service.ts
+++ src/app/services/usuarios.service.ts
@@ -1,11 +1,13 @@
 import { Injectable } from '@angular/core';
-import {environment} from "../../environments/environment";
-import {Usuario} from "../models/usuario.model";
-import {HttpClient} from "@angular/common/http";
-import {Router} from "@angular/router";
+import { HttpClient } from '@angular/common/http';
+import { tap, map, catchError } from 'rxjs/operators';
+import { Router } from '@angular/router';
+import { Observable, of } from 'rxjs';
+
+import { environment } from '../../environments/environment';
 import {RegisterForm} from "../interfaces/register-form.interface";
-import {tap} from "rxjs";
 import {LoginForm} from "../interfaces/login-form.interface";
+import {Usuario} from "../models/usuario.model";
 
 const base_url = environment.base_url;
 
@@ -48,6 +50,23 @@ export class UsuariosService {
           this.guardarLocalStorage('token', resp.token )
         })
       );
+
+  }
+
+  validarToken(): Observable<boolean> {
+    const token = localStorage.getItem('token') || '';
+
+    return this.http.get(`${ base_url }/renew`, {
+      headers: {
+        'x-token': token
+      }
+    }).pipe(
+      tap( (resp: any) => {
+        localStorage.setItem('token', resp.token );
+      }),
+      map( resp => true),
+      catchError( error => of(false) )
+    );
 
   }
 
