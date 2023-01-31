1cfac1be7e026753b0ecc2a7630ff940bb6587f0
Palacios Tarrillo Christian <85316618+PalaciosTarrilloChristian@users.noreply.github.com>
2023-01-31 18:53:10 -0500

    Efecto Hover añadido

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -4,12 +4,11 @@ import { CommonModule } from '@angular/common';
 import { PagesRoutingModule } from './pages-routing.module';
 import {PagesComponent} from "./pages.component";
 import {SharedModule} from "../shared/shared.module";
-import {RouterModule, RouterOutlet} from "@angular/router";
-import {BrowserModule} from "@angular/platform-browser";
+import {RouterOutlet} from "@angular/router";
 import {PersonajesComponent} from "./personajes/personajes.component";
 import { RegistrarPersonajeComponent } from './registrar-personaje/registrar-personaje.component';
-import {AuthRoutingModule} from "../auth/auth-routing.module";
-import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+
+import {ReactiveFormsModule} from "@angular/forms";
 import {HttpClientModule} from "@angular/common/http";
 import { PersonajesRegistradosComponent } from './personajes-registrados/personajes-registrados.component';
 import { UsuarioComponent } from './mantenimiento/usuario/usuario.component';
